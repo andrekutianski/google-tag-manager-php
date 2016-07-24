0cace830eb01ec4a0a8b955172d76f617f21fec4
Antonio Tajuelo <atajuelo@gmail.com>
2016-07-24 19:33:11 +0200

    First commit!! :)

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # google-tag-manager-php
 Google Tag Manager PHP Helper
+
+This class provides some simple PHP helper functions for implementing Google Tag Manager within your website.
+
+## Installation via Composer
+to be done
+
+## Methods:
+- Gtm::renderContainer($container_id,$datalayer_items_array)
+- Gtm::datalayerPush($datalayer_items_array)
+- Gtm::datalayerRemember($datalayer_items_array)
+
+## Method renderContainer
+
+#### Description
+Renders a Google Tag Manager container.
+
+#### Parameters
+- $container_id (required): The id of your container. You can get this value from your Google Tag Manager account admin panel.
+- $datalayer_items_array (optional): Array of key-value items to be added to the datalayer.
+
+## Method datalayerPush
+
+#### Description
+Use this method for populating your datalayer. When this method is called BEFORE the container rendering, it will populate the datalayer at the time it is declared in the HTML code. When this method is called AFTER the container has been rendered, it will render JavaScript ```dataLayer.push({...})``` calls.
+
+#### Parameters
+- $datalayer_items_array (required): Array of key-value items to be added to the datalayer.
+
+## Method datalayerRemember
+
+#### Description
+This method is intended for collecting datalayer information in those parts of your application not rendering HTML code. All datalayer items added through this method would be reminded until a container is rendered on a HTML page, when they would finally be triggered.
+
+#### Parameters
+- $datalayer_items_array (required): Array of key-value items to be added to the datalayer.
---
+++ src/Gtm.php
@@ -0,0 +1,57 @@
+<?php namespace AntonioTajuelo\Gtm;
+
+class Gtm {
+
+  private static $datalayer = [];
+
+  private static $is_rendered  = false;
+
+  public static function renderContainer($container_id,$datalayer = []) {
+
+    self::$is_rendered = true;
+
+    if(isset($_SESSION['datalayer'])) {
+      if(count($_SESSION['datalayer']) > 0) {
+        self::$datalayer = array_merge($_SESSION['datalayer'],self::$datalayer);
+        $_SESSION['datalayer'] = [];
+      }
+    }
+
+    if(count($datalayer) > 0) {
+      echo '<script>var dataLayer = [';
+      echo json_encode(array_merge($datalayer,self::$datalayer),JSON_UNESCAPED_SLASHES);
+      echo '];</script>';
+      echo "\n";
+    }
+
+    echo '<noscript><iframe src="//www.googletagmanager.com/ns.html?id=' . $container_id . '"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
+new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
+\'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,\'script\',\'dataLayer\',\'' . $container_id . '\');</script>' . "\n";
+
+  }
+
+  public function datalayerPush($items = []) {
+
+    if(self::$is_rendered) {
+      echo '<script>dataLayer.push(' . json_encode($items,JSON_UNESCAPED_SLASHES) . ');</script>' . "\n";
+    } else {
+      self::$datalayer = array_merge(self::$datalayer,$items);
+    }
+
+  }
+
+  public function datalayerRemember($items) {
+
+    if(!isset($_SESSION['datalayer'])) {
+      $_SESSION['datalayer'] = [];
+    }
+
+    $_SESSION['datalayer'] = array_merge($_SESSION['datalayer'],$items);
+
+  }
+
+}
