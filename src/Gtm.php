2a62f5eba62c2d32687b9d9bf434845c91d7e71b
Antonio Tajuelo <atajuelo@gmail.com>
2017-08-22 08:54:41 +0200

    Update Gtm.php

--- src/Gtm.php
+++ src/Gtm.php
@@ -6,7 +6,7 @@ class Gtm {
 
   private static $is_rendered  = false;
 
-  public static function renderContainer($container_id,$datalayer = []) {
+  public static function renderContainer($container_id,$datalayer = [],$options = []) {
 
     self::$is_rendered = true;
 
@@ -24,13 +24,19 @@ class Gtm {
       echo "\n";
     }
 
-    echo '<noscript><iframe src="//www.googletagmanager.com/ns.html?id=' . $container_id . '"
+    $output = '<noscript><iframe src="//www.googletagmanager.com/ns.html?id=' . $container_id . '"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
 new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
+j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;';
+    if(in_array('defer',$options)) {
+      $output .= 'j.defer=true;';
+    }
+    $output .= 'j.src=
 \'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,\'script\',\'dataLayer\',\'' . $container_id . '\');</script>' . "\n";
+    
+    echo $output;
 
   }
 
