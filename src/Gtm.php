f112e10ae8528f850bca6577fd655b02c452677c
Antonio Tajuelo <atajuelo@gmail.com>
2017-08-22 12:52:37 +0200

    Update Gtm.php

--- src/Gtm.php
+++ src/Gtm.php
@@ -28,9 +28,11 @@ class Gtm {
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
 new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;';
+j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';';
     if(in_array('defer',$options)) {
       $output .= 'j.defer=true;';
+    } else {
+      $output .= 'j.async=true;';
     }
     $output .= 'j.src=
 \'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
